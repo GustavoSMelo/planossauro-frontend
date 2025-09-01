d387ee65a8553c192b18765482051365809a1cfb
GustavoSMelo <gsantos15569@gmail.com>
2025-09-01 00:23:49 -0300

    :tada: :sparkles: project started and LP done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import './styles/app.style.scss';
+</script>
+
+<template>
+    <div class="containerLP">
+        <div class="wrapper">
+            <navbar class="navbar">
+                <figure class="dinoLogo">
+                    <img src="./assets/dino2.svg" alt="Planeja.ai dino logo" />
+                </figure>
+
+                <button>
+                    <img src="./assets/dino2.svg" alt="imageTheme" />
+                </button>
+            </navbar>
+
+            <main class="mainContent">
+                <div>
+                    <h1>Projeta.ai</h1>
+                    <h2>Seu planejador pessoal</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nobis rem, perspiciatis nesciunt
+                        nulla
+                        mollitia quam fugit veniam et veritatis asperiores libero corrupti magnam. Molestias tenetur
+                        saepe
+                        laborum porro eveniet!
+                    </p>
+
+                    <button class="btnLogin" type="button">Login</button>
+                    <button class="btnExamples" type="button">Ver examples</button>
+                </div>
+
+                <figure>
+                    <img src="./assets/kids.png" alt="kids" />
+                </figure>
+            </main>
+        </div>
+    </div>
+</template>
