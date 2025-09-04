dc0a65f13126a43c02f5a76efa578de87afddd0e
GustavoSMelo <gsantos15569@gmail.com>
2025-09-04 01:32:50 -0300

    :sparkles: component navbar and footer done

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import './footer.style.scss';
+</script>
+
+<template>
+    <footer>
+        <h3><a href="https://github.com/GustavoSMelo/planeja.ai-frontend">Desenvolvido por Gustavo Santos Melo</a></h3>
+    </footer>
+</template>
