09742832be896108fd4ed65e98d992130a1d1e3b
GustavoSMelo <gsantos15569@gmail.com>
2025-10-22 23:34:14 -0300

    :construction: working on preview docx page

---
+++ src/components/previewDocs/PreviewDocs.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import './previewdocs.style.scss';
+</script>
+
+<template>
+    <div class="showPreviewContainer">
+        <iframe class="showPreview" src="../../../public/planejamentoSemanal1.pdf"></iframe>
+    </div>
+</template>
