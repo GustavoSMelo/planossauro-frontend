dbda78470bd902682c56c519854a736520a30f88
GustavoSMelo <gsantos15569@gmail.com>
2025-11-18 22:35:06 -0300

    :monocle_face: thinking in UI/UX

--- src/views/contents/HomeContent.vue
+++ src/views/contents/HomeContent.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="homeContentContainer">
-        <img src="../../assets/dino_party.png" alt="Dino Party" />
+    <div class="homeContentContainerTitle">
+        <img src="../../assets/profileDino.png" alt="Dino Party" />
         <h1>
             Voce esta na pagina inicial <br />
             Para utilizar nosso sistema, use as abas
         </h1>
     </div>
+    <section class="infoContainer">
+        <div class="planInfoContainer">
+            <h2>Seu plano atual:</h2>
+
+            <img src="../../assets/dino_premium.png" alt="dino premium" />
+
+            <h4>Premium</h4>
+            <button type="button">Acessar plano</button>
+        </div>
+        <div class="planningsInfoContainer">
+            <h2>Seus planejamentos:</h2>
+
+            <img src="../../assets/DinoConsultaPlanejamentos.png" alt="dino premium" />
+
+            <h4 class="first">Semanais: 25/100</h4>
+            <h4>Mensais: 24/100</h4>
+            <button type="button">Ver planejamentos</button>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped src="../../styles/contents/homecontent.style.scss" />
