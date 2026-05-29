7c1c236a0b37a1300651e8e1299da2f58833c520
GustavoSMelo <gsantos15569@gmail.com>
2026-05-29 00:59:30 -0300

    :loud_sound: add error message popup

--- src/views/callbacks/FacebookCallback.vue
+++ src/views/callbacks/FacebookCallback.vue
@@ -1,50 +1,61 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { inject, onMounted } from "vue";
 import { useI18n } from "vue-i18n";
 import backendApi from "../../api/api";
 import router from "../../routes";
 import type { IFacebookCallback } from "../../interfaces/facebookCallback.interface";
+import type { IPopupContext } from "../../interfaces/context/popup.interface";
 import { setToken } from "../../helpers/token";
 
 const { t } = useI18n();
 
 onMounted(async () => {
+    const popupContext: IPopupContext = inject("popup") as IPopupContext;
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
 
-    const response = await backendApi.get(`/token/facebook/${code}`);
+    try {
+        const response = await backendApi.get(`/token/facebook/${code}`);
 
-    if (response.status === 200) {
-        const token = response.data.access_token;
-        const facebookResponse = (
-            await backendApi.get(`/auth/facebook/${token}`)
-        ).data as IFacebookCallback;
+        if (response.status === 200) {
+            const token = response.data.access_token;
+            const facebookResponse = (
+                await backendApi.get(`/auth/facebook/${token}`)
+            ).data as IFacebookCallback;
 
-        if (!facebookResponse.token.plainTextToken.length) {
+            if (!facebookResponse.token.plainTextToken.length) {
+                sessionStorage.setItem("loginType", "facebook");
+                sessionStorage.setItem("facebookEmail", facebookResponse.email);
+                sessionStorage.setItem("facebookId", facebookResponse.id);
+                sessionStorage.setItem("fullName", facebookResponse.name);
+                sessionStorage.setItem("facebookAccessToken", token);
+
+                return router.push(`/finish/login`);
+            }
+
+            setToken(facebookResponse.token.plainTextToken);
+            sessionStorage.setItem("user", JSON.stringify(facebookResponse.user));
             sessionStorage.setItem("loginType", "facebook");
-            sessionStorage.setItem("facebookEmail", facebookResponse.email);
-            sessionStorage.setItem("facebookId", facebookResponse.id);
-            sessionStorage.setItem("fullName", facebookResponse.name);
-            sessionStorage.setItem("facebookAccessToken", token);
 
-            return router.push(`/finish/login`);
+            const responseHour = await backendApi.get(
+                `/planninghour/${facebookResponse.user.uuid}`,
+            );
+            sessionStorage.setItem("initial_hour", responseHour.data.initial_hour);
+            sessionStorage.setItem(
+                "interval",
+                responseHour.data.interval_between_classes,
+            );
+
+            router.push("/app");
+            return;
         }
-
-        setToken(facebookResponse.token.plainTextToken);
-        sessionStorage.setItem("user", JSON.stringify(facebookResponse.user));
-        sessionStorage.setItem("loginType", "facebook");
-
-        const responseHour = await backendApi.get(
-            `/planninghour/${facebookResponse.user.uuid}`,
+    } catch {
+        popupContext.handleChangePopupInfo(
+            t("facebookcallback.loginError"),
+            "error",
+            true,
         );
-        sessionStorage.setItem("initial_hour", responseHour.data.initial_hour);
-        sessionStorage.setItem(
-            "interval",
-            responseHour.data.interval_between_classes,
-        );
-
-        router.push("/app");
-        return;
+        router.push("/login");
     }
 });
 </script>
